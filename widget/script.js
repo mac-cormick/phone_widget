36dae3e7d252ce6dce13d54d2a5bd888c8575b02
mac-cormick <mac-cormick@yandex.ru>
2018-01-15 00:06:19 +0300

    error fixed

--- widget/script.js
+++ widget/script.js
@@ -1,25 +1,25 @@
 define(['jquery'], function($){
-    var CustomWidget = function () {
-    	var self = this;
+	var CustomWidget = function () {
+		var self = this;
 		this.callbacks = {
 			render: function(){
 				return true;
 			},
-			init: function(){
+			init: function() {
 				self.add_action('phone', function(data) {
-					self.crm.post (
-						'http://127.0.0.1/file.php',
+					self.crm_post (
+						'http://localhost/amophone/file.php',
 						{
 							call_to: data.value
 						},
 						function (msg) {
-							alert('Данные отправлены')ж
+							alert('Данные отправлены');
 						},
 						'text',
 						function() {
 							alert('Error');
 						}
-					);
+						);
 				});
 			},
 			bind_actions: function(){
@@ -41,21 +41,21 @@ define(['jquery'], function($){
 						console.log('contacts');
 					}
 				},
-			leads: {
+				leads: {
 					//select leads in list and clicked on widget name
 					selected: function(){
 						console.log('leads');
 					}
 				},
-			tasks: {
+				tasks: {
 					//select taks in list and clicked on widget name
 					selected: function(){
 						console.log('tasks');
 					}
 				}
+			};
+			return this;
 		};
-		return this;
-    };
 
-return CustomWidget;
-});
+		return CustomWidget;
+	});
