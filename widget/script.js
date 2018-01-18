ac32304e56c92b18b51f72d99b7669c1a219e846
mac-cormick <mac-cormick@yandex.ru>
2018-01-18 17:57:08 +0300

    laduhfialhdfl

--- widget/script.js
+++ widget/script.js
@@ -21,21 +21,54 @@ define(['jquery'], function($){
 						}
 						);
 				});
-			},
-			bind_actions: function(){
-				return true;
-			},
-			settings: function(){
-				return true;
-			},
-			onSave: function(){
-				alert('click');
-				return true;
-			},
-			destroy: function(){
+				self.add_call_notify = function(data){
+					var w_name = self.i18n('widget').name,
+					date_now = Math.ceil(Date.now()/1000),
+					lang = self.i18n('settings'),
+					n_data = {
+						from: data.from,
+						to: data.to,
+						duration: data.duration,
+						link: data.link,
+						text: w_name + ': ' + data.text,
+						date: date_now,
+						element: data.element
+					};
+					/* Делаем проверку, существует ли ID контакта, совершающего входящий вызов */
+        if (n_data.element.id > 0){     //Если ID существует, формируем ссылку на данный контакт в amoCRM
+        	text = 'Вам звонит: '+n_data.element.name+'</br><a href="/contacts/detail/'+ n_data.element.id+'"> Перейти в карту контакта</a>';
+        	n_data.text = text;
+        	n_data.from = data.from;
+        	if (n_data.from.length < 4){   //Проверка на внутренний номер
+        		n_data.header = 'Внутренний номер: '+data.from+'';
+        	}
+        	else {
+        		n_data.header = 'Входящий вызов: '+data.from+'';
+        	}
+        }
+        AMOCRM.notifications.add_call(n_data);
+      };
+      /* Далее данные, имитирующие поступающую информацию  */
+      var notify_data={};
+      notify_data.from = '+7 (999) 111 22 33';
+      notify_data.to = 'Моляков Анатолий';
+      notify_data.element = { id: 3923091, type: "contact" };
+      self.add_call_notify(notify_data);
+    },
+    bind_actions: function(){
+    	return true;
+    },
+    settings: function(){
+    	return true;
+    },
+    onSave: function(){
+    	alert('click');
+    	return true;
+    },
+    destroy: function(){
 
-			},
-			contacts: {
+    },
+    contacts: {
 					//select contacts in list and clicked on widget name
 					selected: function(){
 						console.log('contacts');
